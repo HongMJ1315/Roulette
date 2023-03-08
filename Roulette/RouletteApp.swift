99cb942d6b1e4b3b22c2caa62e1d493aebfb0853
HongMJ1315 <gone_co_1008@yahoo.com.tw>
2023-03-08 11:12:11 +0800

    Initial Commit

---
+++ Roulette/RouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouletteApp.swift
+//  Roulette
+//
+//  Created by Mr.JB on 2023/3/8.
+//
+
+import SwiftUI
+
+@main
+struct RouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
